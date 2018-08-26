241261ad97354d2f5e59f3bfa04a72f3dd4b6c0d
Neil Allain <njallain@bluerope.org>
2018-08-26 06:01:29 -0500

    fix feed

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Back to Blogging",
         "excerpt":"It’s been a long time since I’ve blogged, and I never really good about keeping it consistent before anyway. I’m going to give it another shot to see it I can provide anything useful to anyone else. In general I’ll be writing mostly about software development, but also some about...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2018/08/08/back-to-blogging.html",
+        "url": "https://bluerope.org/2018/08/08/back-to-blogging.html",
         "teaser":null},{
         "title": "Journey to ECS",
         "excerpt":"While I’m a developer by day, I don’t do much iOS development anymore and certainly no game development. I’ve had many ideas for games in the past and have toyed with them, but I’ve finally started working a bit more on a particular idea. Part of this is to improve...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2018/08/11/journey-to-ecs.html",
+        "url": "https://bluerope.org/2018/08/11/journey-to-ecs.html",
         "teaser":null},{
         "title": "Eat. Race. Win.",
         "excerpt":"I’m almost through watching this on Amazon Prime. It’s coverving the 2017 Tour de France with Orica Scott, and focusing on how their chef feeds them for 3 weeks. It combines 2 of the things I love: cycling and eating. Very enjoyable so far. The biggest negative is that the...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2018/08/13/eat-race-win.html",
+        "url": "https://bluerope.org/2018/08/13/eat-race-win.html",
         "teaser":null},{
         "title": "The 'E' in ECS",
         "excerpt":"In ECS, entities are just an identifier which components will be attached to, usually just an integer. Rather than just a typedef for Int, I decided to make Entity a struct to provide a bit more type safety. I’m trusting in the swift compiler to make this reasonably efficient. struct...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2018/08/23/the-e-in-ecs.html",
+        "url": "https://bluerope.org/2018/08/23/the-e-in-ecs.html",
         "teaser":null},{
         "title": "VUELTA BEGINS!",
         "excerpt":"Vuelta Espana started today. An 8k TT with a surprisingly bad performance from Richie Porte. Did I miss some news about him being sick or something? I wouldn’t expect him to beat Rohan Dennis, but to lose 21 seconds on Quintana is pretty shocking. Will Kwiatkowski go for GC even...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2018/08/25/vuelta-starts.html",
+        "url": "https://bluerope.org/2018/08/25/vuelta-starts.html",
         "teaser":null}]
