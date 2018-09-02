cf7a08c771c2951e7abf85d784a7621243b4d4fd
Neil Allain <njallain@bluerope.org>
2018-09-02 14:39:50 -0500

    message

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,33 +4,38 @@ var store = [{
         "tags": [],
         "url": "https://bluerope.org/2018/08/08/back-to-blogging.html",
         "teaser":null},{
-        "title": "Journey to ECS",
+        "title": "Journey to Swift ECS",
         "excerpt":"While I’m a developer by day, I don’t do much iOS development anymore and certainly no game development. I’ve had many ideas for games in the past and have toyed with them, but I’ve finally started working a bit more on a particular idea. Part of this is to improve...","categories": [],
-        "tags": [],
+        "tags": ["swift"],
         "url": "https://bluerope.org/2018/08/11/journey-to-ecs.html",
         "teaser":null},{
         "title": "Eat. Race. Win.",
         "excerpt":"I’m almost through watching this on Amazon Prime. It’s coverving the 2017 Tour de France with Orica Scott, and focusing on how their chef feeds them for 3 weeks. It combines 2 of the things I love: cycling and eating. Very enjoyable so far. The biggest negative is that the...","categories": [],
-        "tags": [],
+        "tags": ["cycling"],
         "url": "https://bluerope.org/2018/08/13/eat-race-win.html",
         "teaser":null},{
         "title": "The 'E' in ECS",
         "excerpt":"In ECS, entities are just an identifier which components will be attached to, usually just an integer. Rather than just a typedef for Int, I decided to make Entity a struct to provide a bit more type safety. I’m trusting in the swift compiler to make this reasonably efficient. struct...","categories": [],
-        "tags": [],
+        "tags": ["swift"],
         "url": "https://bluerope.org/2018/08/23/the-e-in-ecs.html",
         "teaser":null},{
         "title": "VUELTA BEGINS!",
         "excerpt":"Vuelta Espana started today. An 8k TT with a surprisingly bad performance from Richie Porte. Did I miss some news about him being sick or something? I wouldn’t expect him to beat Rohan Dennis, but to lose 21 seconds on Quintana is pretty shocking. Will Kwiatkowski go for GC even...","categories": [],
-        "tags": [],
+        "tags": ["cycling"],
         "url": "https://bluerope.org/2018/08/25/vuelta-starts.html",
         "teaser":null},{
         "title": "C comes after E?",
         "excerpt":"On the surface components are fairly simple. In ECS, a component, is basically a set of properties that an entity has. So if the entity is located somewhere, it might have a Location component with x and y coordinates. In fact, even though I’m add a protocol for Component, there’s...","categories": [],
-        "tags": [],
+        "tags": ["swift"],
         "url": "https://bluerope.org/2018/08/28/components.html",
         "teaser":null},{
         "title": "Huge win for Ben King!",
         "excerpt":"I’m so excited to see Ben King win a stage in this year’s Vuelta.  A truly epic ride. And he posted his full ride to Strava, so geeks like me can be amazed. :bicyclist: ","categories": [],
-        "tags": [],
+        "tags": ["cycling"],
         "url": "https://bluerope.org/2018/08/29/vuelta-stage-4.html",
+        "teaser":null},{
+        "title": "Another Vuelta Win for King",
+        "excerpt":"Can’t believe he got another win in the Vuelta.  He looked absolutely trashed after the stage.Luckily tomorrow is a rest day. Once again, he posted his ride to Strava.I like how left the activity title as “Lunch Ride”. :bicyclist: ","categories": [],
+        "tags": ["cycling"],
+        "url": "https://bluerope.org/2018/09/02/another-vuelta-win.html",
         "teaser":null}]
