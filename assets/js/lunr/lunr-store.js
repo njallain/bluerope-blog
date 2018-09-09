f9abff3ecfa037627320ad6330a25d1ebdfaaf9e
Neil Allain <njallain@bluerope.org>
2018-09-09 07:06:10 -0500

    message

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser":null},{
         "title": "Journey to Swift ECS",
         "excerpt":"While I’m a developer by day, I don’t do much iOS development anymore and certainly no game development. I’ve had many ideas for games in the past and have toyed with them, but I’ve finally started working a bit more on a particular idea. Part of this is to improve...","categories": [],
-        "tags": ["swift"],
+        "tags": ["swift","ecs"],
         "url": "https://bluerope.org/2018/08/11/journey-to-ecs.html",
         "teaser":null},{
         "title": "Eat. Race. Win.",
@@ -16,7 +16,7 @@ var store = [{
         "teaser":null},{
         "title": "The 'E' in ECS",
         "excerpt":"In ECS, entities are just an identifier which components will be attached to, usually just an integer. Rather than just a typedef for Int, I decided to make Entity a struct to provide a bit more type safety. I’m trusting in the swift compiler to make this reasonably efficient. struct...","categories": [],
-        "tags": ["swift"],
+        "tags": ["swift","ecs"],
         "url": "https://bluerope.org/2018/08/23/the-e-in-ecs.html",
         "teaser":null},{
         "title": "VUELTA BEGINS!",
@@ -26,7 +26,7 @@ var store = [{
         "teaser":null},{
         "title": "C comes after E?",
         "excerpt":"On the surface components are fairly simple. In ECS, a component, is basically a set of properties that an entity has. So if the entity is located somewhere, it might have a Location component with x and y coordinates. In fact, even though I’m add a protocol for Component, there’s...","categories": [],
-        "tags": ["swift"],
+        "tags": ["swift","ecs"],
         "url": "https://bluerope.org/2018/08/28/components.html",
         "teaser":null},{
         "title": "Huge win for Ben King!",
@@ -41,11 +41,11 @@ var store = [{
         "teaser":null},{
         "title": "ECS Breakout",
         "excerpt":"Like I mentioned last time, properly designing and explaining an ECS is best if done in the context of a game.I am actually designing a game while doing it, but for explaining it I want a very simple game. Breakout is very simple (and is one of the first games...","categories": [],
-        "tags": ["swift"],
+        "tags": ["swift","ecs"],
         "url": "https://bluerope.org/2018/09/04/ecs-breakout.html",
         "teaser":null},{
         "title": "Simple Systems",
         "excerpt":"One of the main reasons I decided to include a game example while documenting the ECS development isthat I found that much of it is about the pattern that I use with it rather than the actual code itself. The basic idea of a system in ECS is that it...","categories": [],
-        "tags": ["swift"],
+        "tags": ["swift","ecs"],
         "url": "https://bluerope.org/2018/09/07/simple-systems.html",
         "teaser":null}]
